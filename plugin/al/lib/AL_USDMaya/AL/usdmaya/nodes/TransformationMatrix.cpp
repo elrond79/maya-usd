sopvop/AL_USDMaya

fix setting of xform attrs on proxy creation

lib/AL_USDMaya/AL/usdmaya/nodes/TransformationMatrix.cpp
@@ -102,18 +102,18 @@ TransformationMatrix::TransformationMatrix(const UsdPrim& prim)
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void TransformationMatrix::setPrim(const UsdPrim& prim)
+void TransformationMatrix::setPrimInternal(const UsdPrim& prim)
 {
   if(prim.IsValid())
   {
-    TF_DEBUG(ALUSDMAYA_EVALUATION).Msg("TransformationMatrix::setPrim %s\n", prim.GetName().GetText());
+    TF_DEBUG(ALUSDMAYA_EVALUATION).Msg("TransformationMatrix::setPrimInternal %s\n", prim.GetName().GetText());
     m_prim = prim;
     UsdGeomXform xform(prim);
     m_xform = xform;
   }
   else
   {
-    TF_DEBUG(ALUSDMAYA_EVALUATION).Msg("TransformationMatrix::setPrim null\n");
+    TF_DEBUG(ALUSDMAYA_EVALUATION).Msg("TransformationMatrix::setPrimInternal null\n");
     m_prim = UsdPrim();
     m_xform = UsdGeomXform();
   }

elrond79/maya-usd

plugin/al/lib/AL_USDMaya/AL/usdmaya/nodes/TransformationMatrix.cpp
@@ -120,19 +120,19 @@ TransformationMatrix::TransformationMatrix(const UsdPrim& prim)
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void TransformationMatrix::setPrim(const UsdPrim& prim, Scope* transformNode)
+void TransformationMatrix::setPrimInternal(const UsdPrim& prim, Scope* transformNode)
 {
   m_enableUsdWriteback = false;
   if(prim.IsValid())
   {
-    TF_DEBUG(ALUSDMAYA_TRANSFORM_MATRIX).Msg("TransformationMatrix::setPrim %s\n", prim.GetName().GetText());
+    TF_DEBUG(ALUSDMAYA_TRANSFORM_MATRIX).Msg("TransformationMatrix::setPrimInternal %s\n", prim.GetName().GetText());
     m_prim = prim;
     UsdGeomXformable xform(prim);
     m_xform = xform;
   }
   else
   {
-    TF_DEBUG(ALUSDMAYA_TRANSFORM_MATRIX).Msg("TransformationMatrix::setPrim null\n");
+    TF_DEBUG(ALUSDMAYA_TRANSFORM_MATRIX).Msg("TransformationMatrix::setPrimInternal null\n");
     m_prim = UsdPrim();
     m_xform = UsdGeomXformable();
   }